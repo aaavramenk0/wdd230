7d0fe4eaf6e42f0d670b1313dcc682d1507d29f9
Aleksandr <alex.avramenko2004@gmail.com>
2023-01-06 10:09:48 -0500

    Media queries

--- scripts/getDates.js
+++
@@ -1,3 +0,0 @@
-let lastVisitDate = new Date(document.lastModified);
-let lastVisitYear = lastVisitDate.getFullYear()
-const date = document.getElementById('lastModified').innerHTML = lastVisitYear;
