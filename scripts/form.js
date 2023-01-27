f95e35718c6e520f7ffc649951f71e8e5d7623ef
Aleksandr <alex.avramenko2004@gmail.com>
2023-01-27 11:39:10 -0500

    Form

--- scripts/form.js
+++ scripts/form.js
@@ -1,17 +1,3 @@
-let username = document.getElementById('username')
-let usernameRepeat = document.getElementById('username-repeat')
-const inputTable = document.getElementById('input-table')
-
-const checkUsernameMatch = () => {
-    
-    if(username === usernameRepeat) {
-        inputTable.style.display = flex;
-        document.querySelector('h2').innerHTML = 2
-    } else {
-        usernameRepeat.focus();
-    }
-}
-
 /* Page rating script */
 const rating = document.getElementById("rating");
 const rangevalue = document.getElementById("page-rating");
