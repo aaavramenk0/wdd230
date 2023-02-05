c038549ecf0de5ff6422883672f6f9cfe8508ac7
Aleksandr <alex.avramenko2004@gmail.com>
2023-02-05 16:00:20 -0500

    Weather on the home page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,1 +1,24 @@
 document.getElementById('lastModified').innerHTML = 'Last Modification: ' + document.lastModified;
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.6371&appid=7cf66b5215db60e56aa4e23f5e4ed5fb&units=imperial';
+const card = document.getElementById('card');
+
+async function getWeather() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json(); 
+            console.log(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function displayResults(data) {
+    
+}
+
+getWeather()
--- scripts/weather.js
+++ scripts/weather.js
@@ -1,21 +1,34 @@
 const weatherType = document.getElementById('weather-type')
-const weatherTemp = document.getElementById('weather-temp')
+const weatherTemp = document.getElementById('weather-temp');
+const weatherIcon = document.getElementById('weatherIcon');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=33.9245&lon=-84.8378&appid=7cf66b5215db60e56aa4e23f5e4ed5fb'
 
-async function getWeather(url) {
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.6371&appid=7cf66b5215db60e56aa4e23f5e4ed5fb&units=imperial'
+
+async function getWeather() {
     try {
         const response = await fetch(url);
         if (response.ok) {
-            const data = await response.json(); 
+            const data = await response.json();
             displayResults(data);
-            console.log(data)
         } else {
             throw Error(await response.text());
         }
     } catch (error) {
         console.log(error)
     }
+}
+
+const displayResults = (data) => {
+    const placeTempF = data.main.temp;
+    const placeTempC = ((placeTempF - 32) * 0.556).toFixed(2);
+    
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    weatherIcon.setAttribute('src', iconsrc)
+    
+
+    weatherTemp.innerText = "Temperature in Dallas, GA: " + placeTempF + "F or " + placeTempC + "C";
+    weatherType.innerText = data.weather[0].main
 }
 
 getWeather()
