ce9fa3a57bca541b073af5b2692460f7ab379303
Aleksandr <alex.avramenko2004@gmail.com>
2023-02-21 11:12:02 -0500

    To dark theme button

--- scripts/light-to-dark.js
+++ scripts/light-to-dark.js
@@ -2,6 +2,8 @@ const toggleButton = document.getElementById('toggle-btn');
 const navbar = document.querySelector('.navbar');
 const navbarLinks = document.querySelectorAll('.navbar a');
 const menuBtn = document.getElementById('menu-btn');
+const card = document.querySelectorAll('.card');
+const footer = document.querySelector('footer')
 
 let isDark;
 
@@ -19,6 +21,11 @@ const toDarkTheme = () => {
     for (let i = 0; i < 3; i++) {
         navbarLinks[i].style.color = 'black';
     }
+    for (let k = 0; k < card.length; k++) {
+        card[k].style.backgroundColor = '#00FF40'
+        card[k].style.color = 'black'
+    }
+    
     menuBtn.style.color = 'black';
     document.body.style.color = 'white';
     document.body.style.backgroundColor = 'black';
